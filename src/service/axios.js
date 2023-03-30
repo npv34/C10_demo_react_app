6f035fdec51400f2304f3e81c5ec853433b9fcb9
npv34 <phanvanluan611996@gmail.com>
2023-03-30 13:16:58 +0700

    create file .env

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8081/api'
+    baseURL: process.env.API_URL
 });
 instance.interceptors.request.use(function (config) {
     const token = localStorage.getItem('token');
